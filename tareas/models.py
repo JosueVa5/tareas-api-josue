92aee43959ff25b6ab0c1dc2f646e4af96ce55cc
Josue <francisco.vargasmedina@cesunbc.edu.mx>
2026-05-30 11:04:49 -0700

    fix: ordenar importaciones con isort

--- tareas/models.py
+++ tareas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Tarea(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField(blank=True)
--- tareas/serializers.py
+++ tareas/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from .models import Tarea
+
 
 class TareaSerializer(serializers.ModelSerializer):
     class Meta:
